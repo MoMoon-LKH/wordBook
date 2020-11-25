e65980302f838da3a70cec02dd3f6d214c7c0b10
MoMoon-LKH <66755342+MoMoon-LKH@users.noreply.github.com>
2020-11-26 07:15:19 +0900

    my 테이블 저장 버튼 중복 여부
    cursor를 통해 my table에 저장되어있는 단어인지 확인 후 Toast 메시지 내용을 다르게 주고 있다면 my table에 저장 X, 없다면 저장 O

--- app/src/main/java/com/example/wordbook/wordScreen.java
+++ app/src/main/java/com/example/wordbook/wordScreen.java
@@ -9,6 +9,7 @@ import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Looper;
+import android.util.Log;
 import android.util.TypedValue;
 import android.view.View;
 import android.widget.Button;
@@ -45,7 +46,7 @@ public class wordScreen extends AppCompatActivity {
         btnBack.setVisibility(View.INVISIBLE);
 
         wordDBHelper = new WordDBHelper(this, WordDBHelper.DB_NAME, null, WordDBHelper.DB_VERSION);
-        db = wordDBHelper.getWritableDatabase();
+        db = wordDBHelper.getWritableDatabase(); //my table에 단어 저장
         Intent intent = getIntent();
 
         String txt = intent.getExtras().getString("txt");
@@ -82,20 +83,32 @@ public class wordScreen extends AppCompatActivity {
         btnMy.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                wordDBHelper.dbInsert(db, WordContract.WordEntry.TABLE_MY,
-                        screenEngTxt.getText().toString(),screenKrTxt.getText().toString());
-                final Toast toast = Toast.makeText(wordScreen.this, "나의 단어장에 저장되었습니다",Toast.LENGTH_SHORT);
-                toast.show();
-                Handler handler = new Handler(Looper.myLooper());
-                handler.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        toast.cancel();
-                    }
-                },900);
+                String eng = screenEngTxt.getText().toString();
+                String kr = screenKrTxt.getText().toString();
+                if(dbWordChecked(eng)){
+                    toastDelay(true);
+                }else{
+                    wordDBHelper.dbInsert(db, WordContract.WordEntry.TABLE_MY, eng, kr);
+                    toastDelay(false);
+                }
+
             }
         });
     }
+
+    boolean dbWordChecked(String eng) { // My 테이블에 이미 있는 단어라면 true / 없다면 false
+        Cursor myCursor = wordDBHelper.recordReadCursorMy(db);
+        while (myCursor.moveToNext()) {
+            Log.d("check","cursor: "+myCursor.getString(myCursor.getColumnIndex(WordContract.WordEntry.COL_ENG)));
+            if (myCursor.getString(myCursor.getColumnIndex(WordContract.WordEntry.COL_ENG)).equals(eng)) {
+                myCursor.close();
+                return true;
+            }
+        }
+        myCursor.close();
+        return false;
+    }
+
 
     private void setCursor(){
         switch (pos) {
@@ -134,6 +147,31 @@ public class wordScreen extends AppCompatActivity {
 
         if(num == 1)
             btnBack.setVisibility(View.INVISIBLE);
+    }
+
+    void toastDelay(boolean check){
+        Handler handler = new Handler(Looper.myLooper());
+
+        if(check) {
+            final Toast toast = Toast.makeText(wordScreen.this, "이미 저장되어있는 단어입니다.", Toast.LENGTH_SHORT);
+            toast.show();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    toast.cancel();
+                }
+            }, 900);
+        } else {
+            final Toast toast = Toast.makeText(wordScreen.this, "나의 단어장에 저장되었습니다", Toast.LENGTH_SHORT);
+            toast.show();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    toast.cancel();
+                }
+            }, 900);
+        }
+
     }
 
     @Override
